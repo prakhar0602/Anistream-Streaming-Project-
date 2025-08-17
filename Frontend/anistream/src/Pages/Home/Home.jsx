2b9cf7c8c496c11eede35aad4279bf4d26757ef8
prakhar0602 <prakhar06092002@gmail.com>
2025-08-18 04:39:02 +0530

    Minor Home Page fix

--- Frontend/anistream/src/Pages/Home/Home.jsx
+++ Frontend/anistream/src/Pages/Home/Home.jsx
@@ -11,7 +11,7 @@ import axios from 'axios'
 const Home = () => {
   const {VITE_BACKEND_LINK}=import.meta.env
   let dispatch=useDispatch();
-  let {series,movies,most_latest}=useSelector(state=>state.local)
+  let {series=[],movies=[],most_latest}=useSelector(state=>state.local)
   const [recommendations, setRecommendations] = useState([])
   
   useEffect(()=>{
@@ -54,24 +54,24 @@ const Home = () => {
         </div>
         )}
         
-        {series && series.length > 0 && (
+        {(series || []).length > 0 && (
           <div className="lg:px-10 lg:py-7 p-5">
             <p className='lg:text-3xl text-2xl lg:mb-7 mb-4 font-funky'>Trending</p>
             <div className='flex gap-3 max-w-full w-full h-fit no-scrollbar overflow-x-scroll overflow-auto'>
             {
-              series.map((s,index)=>(
+              (series || []).map((s,index)=>(
                <Template series={s} key={s.fld_id}/>
                 ))
               }
           </div>
         </div>
         )}
-        {series && series.length > 0 && (
+        {(series || []).length > 0 && (
           <div className="lg:px-10 lg:py-7 p-5">
             <p className='lg:text-3xl text-2xl lg:mb-7 mb-4'>Popular Animes</p>
             <div className='flex gap-3 max-w-full w-full h-fit no-scrollbar overflow-x-scroll overflow-auto'>
             {
-              [...series].sort((a, b) => (b.avg_rating || 0) - (a.avg_rating || 0)).map((s,index)=>(
+              [...(series || [])].sort((a, b) => (b.avg_rating || 0) - (a.avg_rating || 0)).map((s,index)=>(
                 <Link to="/view" onClick={()=>dispatch(select(s))}><Template series={s} key={s.fld_id}/></Link>
                 ))
               }
@@ -101,24 +101,24 @@ const Home = () => {
       </div>
 
 
-        {series && series.length > 0 && (
+        {(series || []).length > 0 && (
           <div className="lg:px-10 lg:py-7 p-5">
             <p className='lg:text-3xl text-2xl lg:mb-7 mb-4'>Anime Series</p>
             <div className='flex gap-3 max-w-full w-full h-fit no-scrollbar overflow-x-scroll overflow-auto'>
             {
-              series.map((s,index)=>(
+              (series || []).map((s,index)=>(
                 <Link to="/view" onClick={()=>dispatch(select(s))}><Template series={s} key={s.fld_id}/></Link>
                 ))
               }
           </div>
         </div>
         )}
-        {movies && movies.length > 0 && (
+        {(movies || []).length > 0 && (
           <div className="lg:px-10 lg:py-7 p-5"> 
             <p className='lg:text-3xl text-2xl lg:mb-7 mb-4'>Anime Movies</p>
             <div className='flex gap-3 max-w-full w-full h-fit no-scrollbar overflow-x-scroll overflow-auto'>
             {
-              movies.map((s,index)=>(
+              (movies || []).map((s,index)=>(
                 <Link to="/view" onClick={()=>dispatch(select(s))}><Template series={s} key={s.fld_id}/></Link>
                 ))
               }
